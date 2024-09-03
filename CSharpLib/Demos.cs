73bb7e170d2073d52b392dd59c76a70a4719032c
Yen Kuo <kuoyen5@gmail.com>
2024-09-03 12:03:01 +0930

    Add project files.

---
+++ CSharpLib/Demos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLib
+{
+    /// <summary>
+    /// This is the CSharp class that will be used in the LanguageComparison project
+    /// </summary>
+    
+    public class Demos
+    {
+        public List<string> LoadFile(){
+            List<string> outputs = [];
+            List<string> lines = File.ReadAllLines(@"C:\temp\test.txt").ToList();
+            for(int i = 0; i < lines.Count; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    outputs.Add(lines[i]);
+                }
+                
+            }
+            return outputs;
+        }
+
+        public void PrintFullName(string firstName, string lastName)
+        {
+            Console.WriteLine($"{firstName} {lastName}");
+        }
+    }
+}
